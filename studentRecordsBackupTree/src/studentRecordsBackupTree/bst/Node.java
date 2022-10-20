a0f263c4627112e4c6707049f0b2a6026327f6cb
vkuncha1 <vkuncha1@binghamton.edu>
2022-10-20 19:52:54 -0400

    class {Node} was Implemented.

--- studentRecordsBackupTree/src/studentRecordsBackupTree/bst/Node.java
+++ studentRecordsBackupTree/src/studentRecordsBackupTree/bst/Node.java
@@ -1,6 +1,74 @@
 
 package studentRecordsBackupTree.bst;
 
+import java.util.ArrayList;
+
 public class Node {
-	
+    //Declaring the Bnumber and description variables
+    int Bnumber;
+    String description;
+    public Node left;
+    public Node right;
+
+
+    /**
+     * Constructor - Initializes the class variables
+     */
+    public Node(){
+        Bnumber = Integer.parseInt(null);
+        description = null;
+        left = null;
+        right = null;
+    }
+
+
+    /**
+     * Getters of the class variables.
+     */
+
+    public int getBnumber(){
+
+        return Bnumber;
+    }
+
+    public String getdescription(){
+
+        return description;
+    }
+
+    public Node getLeft(){
+        return left;
+    }
+
+    public Node getRight(){
+        return right;
+    }
+
+
+
+
+    /**
+     * Setters of the class variables.
+     */
+
+    public void setBnumber(int BnumberIn){
+
+        Bnumber = BnumberIn;
+    }
+
+    public void setDescription(String descriptionIn){
+
+        description = descriptionIn;
+    }
+    public void setLeft(Node leftIn){
+        left = leftIn;
+    }
+
+    public void setRight(Node rightIn){
+        right = rightIn;
+    }
+
+
+
+
 }
